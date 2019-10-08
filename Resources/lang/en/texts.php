ee23e6c952ec4d39b9e15bd86a70a284fbd02db6
Christopher Di Carlo <chris@dicarlosystems.ca>
2019-10-08 08:56:21 -0400

    Updated translations

--- Resources/lang/en/texts.php
+++ Resources/lang/en/texts.php
@@ -2,15 +2,16 @@
 
 $LANG = array(
 
-    'pointofsale' => 'Pointofsale',
-    'pointofsale_list' => 'Pointofsale List',
-    'archive_pointofsale' => 'Archive Pointofsale',
-    'delete_pointofsale' => 'Delete Pointofsale',
-    'edit_pointofsale' => 'Edit Pointofsale',
-    'restore_pointofsale' => 'Restore Pointofsale',
-    'new_pointofsale' => 'New Pointofsale',
-    'created_pointofsale' => 'Successfully created pointofsale',
-    'updated_pointofsale' => 'Successfully updated pointofsale',
+    'pointofsale' => 'Point of Sale',
+    'pointofsale_list' => 'POS Transaction List',
+    'archive_pointofsale' => 'Archive POS purchase',
+    'delete_pointofsale' => 'Delete POS purchase',
+    'edit_pointofsale' => 'Edit Purchase',
+    'restore_pointofsale' => 'Restore POS purchase',
+    'new_pointofsale' => 'New POS purchase',
+    'created_pointofsale' => 'Successfully created POS purchase',
+    'updated_pointofsale' => 'Successfully updated POS purchase',
+    'upc' => 'Scan UPC',
 
 );
 
